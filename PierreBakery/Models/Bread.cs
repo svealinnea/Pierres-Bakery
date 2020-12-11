67f07f06f587e5f2cf84e81cb369d6c652271db5
Svea Wade <svealinneawade@gmail.com>
2020-12-11 11:48:48 -0800

    Added more pastry functionality

--- PierreBakery/Models/Bread.cs
+++ PierreBakery/Models/Bread.cs
@@ -1,16 +1,16 @@
 using System;
+using PierreBakeryUI;
 
 namespace PierreBakery.Models
 {
   public class Bread
   {
     public int Price { get; set;}
-    //public string BreadType { get; set; }
 
     public Bread(int price)
     {
       Price = price;
-      //BreadType = type;
     }
-  }
+
+    }
 }
