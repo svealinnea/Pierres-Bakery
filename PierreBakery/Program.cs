0ed499cbdbfce9e028ba8b4424bbade9bf3df056
Svea Wade <svealinneawade@gmail.com>
2020-12-11 09:51:51 -0800

    Welcome message created for Pierres Delicattesan

---
+++ PierreBakery/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using PierreBakery.Models;
+//using System.Collections.Generic; -- use for lists
+
+namespace PierreBakeryUI
+{
+  public class Program
+  {
+    public static void Main() 
+    { 
+      Console.WriteLine("Welcome to Pierre's Delcious Delicattesan!");
+      Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+      Console.WriteLine("Please press [Enter] to see todays Bread & Pastry prices!");
+      Console.ReadLine();
+    }
+  }
+}
