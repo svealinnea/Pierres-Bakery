e3eb4cd6280bceca01158fc1a0602b14a65a0717
Svea Wade <svealinneawade@gmail.com>
2020-12-11 09:26:54 -0800

    good fail for Pastry class

--- PierreBakery.Tests/Models.Tests/BreadTests.cs
+++ PierreBakery.Tests/Models.Tests/BreadTests.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierreBakery.Models;
 
-namespace ProjectName.Tests
+namespace PierreBakery.Tests
 { 
   [TestClass]
   public class BreadTests
---
+++ PierreBakery.Tests/Models.Tests/PastryTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierreBakery.Models;
+
+namespace PierreBakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreateAnInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry). newPastry.GetType());
+    }
+  }
+}
